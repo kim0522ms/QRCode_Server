c50845f8136f300d1ce16834d38ef7e58bb3d882
kims2 <kims2@MSKim-Home>
2017-12-17 22:46:41 +0900

    Add JSON Library and return function

--- AppInventorQRServer/src/com/example/qr/controller/QRController.java
+++ AppInventorQRServer/src/com/example/qr/controller/QRController.java
@@ -25,6 +25,14 @@ public class QRController extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    public void forwardToViewName(String viewName, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	if(viewName != null) {
+			RequestDispatcher view = request.getRequestDispatcher(viewName);
+			view.forward(request,response);
+		}
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,17 +47,86 @@ public class QRController extends HttpServlet {
 		if (subPath.equals("/rent"))
 		{
 			String clientid = request.getParameter("clientid");
+			String rentDate = request.getParameter("rentdate");
+			String returnDate = request.getParameter("returndate");
 			String itemid = request.getParameter("itemid");
+			
+			System.out.println(clientid + "   " + rentDate + "    " + returnDate + "    " + itemid);
+			
+			int clientid_int = Integer.parseInt(clientid);
+			
+			
+			viewName = "/Rent_Result.html";
 			
 			response.setContentType("text/html; charset=UTF-8");
 			response.setCharacterEncoding("UTF-8");
 			PrintWriter out = response.getWriter();
 			
-			
-			out.println("a" + "    " + "a");
-			out.close();
-			
-			viewName = "/Rent_Result.html";
+			QRDbDao db = new QRDbDao();
+			try {
+				// isExist가 False이면 존재하지 않는 학번
+				boolean isExist = db.checkStudentId(clientid_int);
+				
+				if (isExist == false)
+				{
+					out.print("존재하지 않는 학생입니다!");
+					out.close();
+					
+					forwardToViewName(viewName, request, response);
+					return;
+				}
+				
+				// isOverdue가 True면 연체중
+				boolean isOverdue = db.checkOverdue(clientid_int);
+				
+				if (isOverdue)
+				{
+					out.print("해당 학생은 연체중입니다!");
+					out.close();
+					
+					forwardToViewName(viewName, request, response);
+					return;
+				}
+				
+				// isRented가 true면 이미 대여중인 품목
+				boolean isRented = db.checkRent(itemid);
+				if (isRented)
+				{
+					out.print("이미 대여중인 품목입니다!");
+					out.close();
+					forwardToViewName(viewName, request, response);
+					return;
+				}
+				
+				// isChanged가 false이면 빌릴수 없음.
+				boolean isChanged = db.changeItemStatus(itemid, true);
+				if (isChanged == false)
+				{
+					out.print("알 수 없는 에러 발생...");
+					out.close();
+					forwardToViewName(viewName, request, response);
+					return;
+				}
+				
+				
+				// isSuccess가 True면 대여성공
+				boolean isSuccess = db.enrollmentRent(clientid, rentDate, returnDate, itemid);
+				
+				if (isSuccess)
+				{
+					out.print("Success");
+					out.close();
+				}
+				else
+				{
+					out.print("Failure");
+					out.close();	
+				}
+				
+			} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		else if (subPath.equals("/checkOverdue"))
@@ -57,10 +134,10 @@ public class QRController extends HttpServlet {
 			System.out.println("Request Type : Check Overdue");
 			String clientid = request.getParameter("clientid");
 			QRDbDao db = new QRDbDao();
-			boolean isOk = false;
+			boolean isOverdue = false;
 			
 			try {
-				isOk = db.checkOverdue(Integer.parseInt(clientid));
+				isOverdue = db.checkOverdue(Integer.parseInt(clientid));
 			} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -71,18 +148,101 @@ public class QRController extends HttpServlet {
 			
 			PrintWriter out = response.getWriter();
 			
-			out.println(isOk);
-			
+			System.out.println(isOverdue);
+			out.print(isOverdue);
 			out.close();
 			
 			viewName = "/Rent_Result.html";
 		}
-		
-		
-		if(viewName != null) {
-			RequestDispatcher view = request.getRequestDispatcher(viewName);
-			view.forward(request,response);
+		else if (subPath.equals("/qrcode"))
+		{
+			String itemid = request.getParameter("itemid");
+			String itemName = "";
+			QRDbDao db = new QRDbDao();
+			
+			try {
+				itemName = db.getItemName(itemid);
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			
+			if (itemName.equals(""))
+			{
+				System.out.println("Failed to get item name..");
+				out.print("false");
+				out.close();
+			}
+			else
+			{
+				System.out.println("Item Name is " + itemName);
+				out.print(itemName);
+				out.close();
+			}
+			viewName = "/Rent_Result.html";
 		}
+		
+		else if (subPath.equals("/return"))
+		{
+			String itemid = request.getParameter("itemid");
+			QRDbDao db = new QRDbDao();
+			
+			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			
+			try {
+				boolean isReturned = db.enrollmentReturn(itemid);
+				
+				if (isReturned)
+				{
+					System.out.println("Return Success!");
+					out.print("TRUE");
+					out.close();
+				}
+				else
+				{
+					System.out.println("Return Failed...");
+					out.print("FALSE");
+					out.close();
+				}
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			viewName = "/Rent_Result.html";
+		}
+		else if(subPath.equals("/list"))
+		{
+			QRDbDao db = new QRDbDao();
+			
+			String list = "";
+			try {
+				list = db.getList();
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			out.print(list);
+			out.close();
+			
+			viewName = "/Rent_Result.html";
+		}
+
+		forwardToViewName(viewName, request, response);
+		return;
 	}
 
 	/**
@@ -90,12 +250,11 @@ public class QRController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String viewName = null;
+		String viewName = "/Rent_Result.html";
+
 		
-		if(viewName != null) {
-			RequestDispatcher view = request.getRequestDispatcher(viewName);
-			view.forward(request,response);
-		}
+		forwardToViewName(viewName, request, response);
+		return;
 	}
 
 }
--- AppInventorQRServer/src/com/example/qr/controller/QRDbDao.java
+++ AppInventorQRServer/src/com/example/qr/controller/QRDbDao.java
@@ -6,6 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import sun.tools.jar.resources.jar;
+
 public class QRDbDao {
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
@@ -31,12 +36,35 @@ public class QRDbDao {
 		}
 	}
 	
-	public boolean checkOverdue(int clientid) throws ClassNotFoundException, SQLException {
-		boolean isOk = false;
+	// 존재하는 학번인지 체크
+	public boolean checkStudentId(int clientid) throws SQLException, ClassNotFoundException
+	{
+		boolean isExist = false;
 		
 		connect();
 		
-		String sql = "SELECT * FROM rentinfo WHERE clientid = ? AND returned = 'FALSE'";
+		String sql = "SELECT * FROM CLIENT WHERE clientid = ?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, clientid);
+		ResultSet rs = pstmt.executeQuery();
+		
+		if (rs.next())
+			isExist = true;
+		else
+			isExist = false;
+		
+		disconnect();
+		
+		return isExist;
+	}
+	
+	// 학번을 입력받아서 현재 연체중인지 확인. true일 경우 연체중
+	public boolean checkOverdue(int clientid) throws ClassNotFoundException, SQLException {
+		boolean isOverdue = false;
+		
+		connect();
+		
+		String sql = "SELECT * FROM RENTINFO WHERE clientid = ? AND returndate < SYSDATE AND returned = 'FALSE'";
 		
 		pstmt = conn.prepareStatement(sql);
 		pstmt.setInt(1, clientid);
@@ -44,14 +72,185 @@ public class QRDbDao {
 		ResultSet rs = pstmt.executeQuery();
 		
 		if (rs.next())
+			isOverdue = true;
+		else
+			isOverdue = false;
+		
+		disconnect();
+		
+		return isOverdue;
+	}
+	
+	// DB에 반납정보 기록. 성공시 true 리턴
+	public boolean enrollmentReturn(String itemid) throws SQLException, ClassNotFoundException
+	{
+		boolean result = false;
+		
+		connect();
+		System.out.println("itemid = " + itemid);
+		
+		String sql = "UPDATE items SET rented = 'FALSE' WHERE itemid = ?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, itemid);
+		int isChanged = pstmt.executeUpdate();
+		
+		if (isChanged > 0)
 		{
-			isOk = false;
+			sql = "UPDATE rentinfo SET returned = 'TRUE' WHERE returned = 'FALSE' AND itemid = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, itemid);
+			
+			isChanged = pstmt.executeUpdate();
+			
+			if (isChanged > 0)
+				result = true;
+			else
+				result = false;
 		}
 		else
+			result = false;
+		
+		disconnect();
+		
+		return result;
+	}
+	
+	// DB에 대여정보 기록
+	public boolean enrollmentRent(String clientid, String rentDate, String returnDate, String itemid) throws ClassNotFoundException, SQLException
+	{
+		boolean result = false;
+		
+		connect();
+		
+		String sql = "INSERT INTO rentinfo values (rentInfo_sequence.NEXTVAL, ?, ?, ?, 'FALSE', ?)";
+		
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, clientid);
+		pstmt.setString(2, rentDate);
+		pstmt.setString(3, returnDate);
+		pstmt.setString(4, itemid);
+		
+		int rs = pstmt.executeUpdate();
+		
+		if (rs > 0)
+			result = true;
+		else
+			result = false;
+		
+		disconnect();
+		
+		return result;
+	}
+	
+	public String getItemName(String itemid) throws ClassNotFoundException, SQLException
+	{
+		String itemName = "";
+		
+		connect();
+		
+		String sql = "SELECT * FROM items WHERE itemid = ?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, itemid);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		if (rs.next())
 		{
-			isOk = true;
+			itemName = rs.getString("itemname");
 		}
 		
-		return isOk;
+		disconnect();
+		
+		return itemName;
 	}
+	
+	// 아이템의 대여상태를 변경한다(아이템 id, 변경하고싶은 상태). 변경이 완료되면 true를 리턴함.
+	public boolean changeItemStatus(String itemid, boolean rented) throws SQLException, ClassNotFoundException
+	{
+		boolean isChanged = false;
+		String status = "";
+		
+		if (rented)
+			status = "TRUE";
+		else
+			status = "FALSE";
+		
+		connect();
+		
+		String sql = "UPDATE items SET rented = ? WHERE itemid = ?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, status);
+		pstmt.setString(2, itemid);
+		
+		int result = pstmt.executeUpdate();
+		
+		if (result > 0)
+			isChanged = true;
+		else
+			isChanged = false;
+				
+		disconnect();
+		
+		return isChanged;
+	}
+	
+	public String getList() throws ClassNotFoundException, SQLException
+	{
+		String list = "";
+		JSONObject obj;
+		JSONArray jsonArr = new JSONArray();
+		
+		connect();
+		
+		String sql = "SELECT * FROM RENTINFO r, ITEMS i, client c WHERE RETURNED = 'FALSE' AND r.clientid = c.clientid AND r.itemid = i.itemid";
+		pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		while(rs.next())
+		{
+			obj = new JSONObject();
+			obj.put("itemName", rs.getString("itemname"));
+			obj.put("clientid", rs.getString("clientid"));
+			obj.put("clientName", rs.getString("clientName"));
+			obj.put("rentDate", rs.getString("rentdate"));
+			obj.put("returnDate", rs.getString("returnDate"));
+			
+			jsonArr.put(obj);
+		}
+
+		disconnect();
+		
+		list = jsonArr.toString();
+		
+		System.out.println("JSON Arr : " + list);
+		
+		return list;
+	}
+	
+	// items 테이블을 조회하여 대여중인 item일 경우 true, 아닐 경우 false를 리턴
+	public boolean checkRent(String itemid) throws ClassNotFoundException, SQLException
+	{
+		boolean isRented = false;
+		
+		connect();
+		
+		String sql = "SELECT * FROM items WHERE itemid = ?";
+		
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, itemid);
+		ResultSet rs = pstmt.executeQuery();
+		
+		if (rs.next())
+		{
+			String result = rs.getString("RENTED"); 
+			if(result.equals("TRUE"))
+				isRented = true;
+			else
+				isRented = false;
+		}
+
+		disconnect();
+		
+		return isRented;
+	}
+	
 }
