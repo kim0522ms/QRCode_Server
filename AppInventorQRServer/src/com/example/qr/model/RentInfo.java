ab0047a8b0d8718b744e2dba416790e033644c67
kims2 <kims2@MSKim-Home>
2017-12-17 22:47:42 +0900

    Add jar

---
+++ AppInventorQRServer/src/com/example/qr/model/RentInfo.java
@@ -0,0 +1,26 @@
+package com.example.qr.model;
+
+public class RentInfo {
+	private String itemid;
+	private String itemName;
+	private String status;
+	
+	public String getItemid() {
+		return itemid;
+	}
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
